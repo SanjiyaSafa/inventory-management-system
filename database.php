dee0f04ab98ac4c1e1528b5a68d3f6ae0c8d81a3
SanjiyaSafa <sanjiyasafa05@gmail.com>
2026-08-20 20:21:08 +0600

    Update database.php

--- database.php
+++ database.php
@@ -1,14 +1,14 @@
 <?php
 
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "inventory_db";
+$host = "sql306.infinityfree.com";
+$username = "if0_42697175";
+$password = "Sa12fa45";
+$database = "if0_42697175_XXX";
 
 $conn = new mysqli($host, $username, $password, $database);
 
 if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
+    die("Database connection failed.");
 }
 
 $conn->set_charset("utf8mb4");
